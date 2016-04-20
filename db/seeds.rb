3a8aa47cf97d3def32c44e53f378b8912e3b5632
Tobias Bühlmann <tbuehlmann@users.noreply.github.com>
2016-04-20 22:05:40 +0200

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Product.create(title: 'Raiders of the Lost Ark DVD', price: rand(100), available: [true, false].sample)
+Product.create(title: 'Star Wars 1 DVD', price: rand(100), available: [true, false].sample)
+Product.create(title: 'Star Wars 2 DVD', price: rand(100), available: [true, false].sample)
+Product.create(title: 'Star Wars 3 DVD', price: rand(100), available: [true, false].sample)
+Product.create(title: 'Star Wars 4 DVD', price: rand(100), available: [true, false].sample)
+Product.create(title: 'Star Wars 5 DVD', price: rand(100), available: [true, false].sample)
+Product.create(title: 'Star Wars 6 DVD', price: rand(100), available: [true, false].sample)
+Product.create(title: 'Star Wars 7 DVD', price: rand(100), available: [true, false].sample)
